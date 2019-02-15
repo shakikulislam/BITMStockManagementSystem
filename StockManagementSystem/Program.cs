612c0182f434e7f37746d850b57378a9d6dc9c02
unknown <you@example.com>
2019-02-15 18:01:43 +0600

    View Sales Between Two Dates UI

--- StockManagementSystem/Program.cs
+++ StockManagementSystem/Program.cs
@@ -16,7 +16,7 @@ namespace StockManagementSystem
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new CompanySetupUi());
+            Application.Run(new ViewSalesBetweenTwoDatesUi());
         }
     }
 }
